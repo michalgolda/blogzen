4cfce022e98b39401f6a3b0a798707886857aa7e
michalgolda <michal.golda@hotmail.com>
2024-06-13 20:33:45 +0200

    build: add @nuxt/image package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/test-utils/module"],
+  modules: ["@nuxt/test-utils/module", "@nuxt/image"],
   devtools: { enabled: true },
   postcss: {
     plugins: {

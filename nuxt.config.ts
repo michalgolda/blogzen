afe61d41c3e10d6ece8b580ffd59385844391c11
michalgolda <michal.golda@hotmail.com>
2024-06-13 21:40:52 +0200

    chore: add nuxt-icon module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/test-utils/module", "@nuxt/image"],
+  modules: ["@nuxt/test-utils/module", "@nuxt/image", "nuxt-icon"],
   devtools: { enabled: true },
   postcss: {
     plugins: {

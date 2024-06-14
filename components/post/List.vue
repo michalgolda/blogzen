6061f752661f022c0ed7ab0ce4e39b352908341c
michalgolda <michal.golda@hotmail.com>
2024-06-14 13:57:25 +0200

    feat: create PostList component

---
+++ components/post/List.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4"
+  >
+    <PostCard
+      v-for="(post, index) in data"
+      :key="index"
+      :title="post.title"
+      :tldr="post.tldr"
+      :created-at="post.createdAt"
+      :thumbnail-src="post.thumbnailSrc"
+      :resource-url="post.resourceUrl"
+      :upvotes-count="post.upvotesCount"
+      :views-count="post.viewsCount"
+      :tags="post.tags"
+    />
+  </div>
+</template>
+<script lang="ts" setup>
+export interface PostListProps {
+  data: any[];
+}
+
+const props = defineProps<PostListProps>();
+</script>

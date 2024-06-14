710caf136eef61d259e8a7e0b5912cb41d0e0b3f
michalgolda <michal.golda@hotmail.com>
2024-06-14 13:58:43 +0200

    chore: show some example posts

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>Home page</h1>
+  <PostList :data="data" />
 </template>
+<script setup lang="ts">
+const data = computed(() => [
+  {
+    title: "Frontend resources!",
+    tldr: "A collection of frontend resources for UI creation, performance optimization, and more.",
+    thumbnailSrc: "/thumbnail.webp",
+    createdAt: "12 march 2032",
+    resourceUrl: "/r/asd",
+    tags: ["javascript", "react", "css"],
+    upvotesCount: 123,
+    viewsCount: 64,
+  },
+]);
+</script>

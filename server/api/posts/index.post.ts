daff4073c4b5c9998cd634f09eb686eb688406e8
michalgolda <michal.golda@hotmail.com>
2024-06-15 22:40:45 +0200

    feat: create /api/posts endpoint for post creation

---
+++ server/api/posts/index.post.ts
@@ -0,0 +1,34 @@
+import { prisma } from "@@/prisma/client";
+import { createPostSchema } from "@@/validation/post.schema";
+import type { CreatePostBody } from "@@/validation/post.schema";
+import { withBodyValidation } from "@@/server/withBodyValidation";
+
+export default withBodyValidation(
+  defineEventHandler(async (event) => {
+    const body = await readBody<CreatePostBody>(event);
+
+    const existingTags = await prisma.tag.findMany({
+      select: { id: true },
+      where: {
+        id: {
+          in: body.tags,
+        },
+      },
+    });
+
+    const post = await prisma.post.create({
+      data: {
+        title: body.title,
+        tldr: body.tldr,
+        resourceUrl: body.resourceUrl,
+        thumbnailUrl: "/thumbnail.webp",
+        tags: {
+          connect: existingTags,
+        },
+      },
+    });
+
+    return post;
+  }),
+  createPostSchema
+);

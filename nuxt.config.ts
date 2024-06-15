f1e17686e8a05164bc2a3cf50d34a1b672353d68
michalgolda <michal.golda@hotmail.com>
2024-06-15 22:37:02 +0200

    chore: configure @vee-validate/nuxt package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/test-utils/module", "@nuxt/image", "nuxt-icon"],
+  modules: [
+    "@nuxt/test-utils/module",
+    "@nuxt/image",
+    "nuxt-icon",
+    "@vee-validate/nuxt",
+  ],
   css: ["vue-final-modal/style.css"],
   devtools: { enabled: true },
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+    },
+  },
+  veeValidate: {
+    componentNames: {
+      Field: "VeeField",
+      Form: "VeeForm",
     },
   },
 });

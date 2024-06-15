05c61689fa2b94d4a7ba41837d9bf5245ec0b729
michalgolda <michal.golda@hotmail.com>
2024-06-15 22:27:13 +0200

    chore: use ModalsContainer component

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,5 +4,9 @@
     <main class="container mx-auto px-2 py-8">
       <slot />
     </main>
+    <ModalsContainer />
   </div>
 </template>
+<script setup lang="ts">
+import { ModalsContainer } from "vue-final-modal";
+</script>

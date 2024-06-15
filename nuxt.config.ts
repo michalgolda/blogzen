ffea6bdb1b95f7e297b50efa63c8e7d37f822d77
michalgolda <michal.golda@hotmail.com>
2024-06-15 14:42:59 +0200

    chore: configure vue-final-modal package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxt/test-utils/module", "@nuxt/image", "nuxt-icon"],
+  css: ["vue-final-modal/style.css"],
   devtools: { enabled: true },
   postcss: {
     plugins: {

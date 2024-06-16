30b1c85443c8b82f1be0a9c1301d7d90017831f0
michalgolda <michal.golda@hotmail.com>
2024-06-16 21:03:37 +0200

    chore: redefine commonOptions passed to mount function after change props definitions

--- tests/components/post/card/Body.spec.ts
+++ tests/components/post/card/Body.spec.ts
@@ -7,8 +7,15 @@ describe("PostCardBody component", () => {
     attrs: {
       title: "test test",
       tldr: "test tldr",
-      tags: ["tag1", "tag2"],
-      createdAt: "test created at",
+      tags: [
+        {
+          name: "tag1",
+        },
+        {
+          name: "tag2",
+        },
+      ],
+      createdAt: new Date(),
     },
   };
 
@@ -24,11 +31,11 @@ describe("PostCardBody component", () => {
     const tldr = component.find("p");
 
     expect(tags.length).toBe(2);
-    expect(firstTag.text()).toBe(`#${commonOptions.attrs.tags[0]}`);
-    expect(secondTag.text()).toBe(`#${commonOptions.attrs.tags[1]}`);
+    expect(firstTag.text()).toBe(`#${commonOptions.attrs.tags[0].name}`);
+    expect(secondTag.text()).toBe(`#${commonOptions.attrs.tags[1].name}`);
 
     expect(createdAt.exists()).toBeTruthy();
-    expect(createdAt.text()).toBe(commonOptions.attrs.createdAt);
+    expect(createdAt.text()).toBe(commonOptions.attrs.createdAt.toDateString());
 
     expect(title.exists()).toBeTruthy();
     expect(title.text()).toBe(commonOptions.attrs.title);
@@ -45,7 +52,7 @@ describe("PostCardBody component", () => {
         <div class="flex flex-row gap-2 mb-1">
           <div class="badge badge-secondary font-semibold rounded text-white border-none gap-2"> #tag1</div>
           <div class="badge badge-secondary font-semibold rounded text-white border-none gap-2"> #tag2</div>
-        </div><span class="text-sm text-gray-500">test created at</span>
+        </div><span class="text-sm text-gray-500">Sun Jun 16 2024</span>
         <h3 class="card-title line-clamp-3">test test</h3>
         <p class="line-clamp-3 mt-1 text-gray-700">test tldr</p>
       </div>"

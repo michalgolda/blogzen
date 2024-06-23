b8a09670231b2940effabdb73df8f4d17e3e9ce8
michalgolda <michal.golda@hotmail.com>
2024-06-23 22:34:56 +0200

    fix: wrong redirect location after sign in using third party provider on production

--- composables/useSignInWithOAuth.ts
+++ composables/useSignInWithOAuth.ts
@@ -1,9 +1,13 @@
 import type { Provider } from "@supabase/supabase-js";
+import { getRedirectUrl } from "@@/utils/getRedirectUrl";
 
 export const useSignInWithOAuth = (provider: Provider) => {
   const supabaseClient = useClientSideSupabaseClient();
   return () =>
     supabaseClient.auth.signInWithOAuth({
       provider,
+      options: {
+        redirectTo: getRedirectUrl(),
+      },
     });
 };
---
+++ utils/getRedirectUrl.ts
@@ -0,0 +1,11 @@
+export const getRedirectUrl = () => {
+  let url: string | undefined =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? process?.env?.NEXT_PUBLIC_VERCEL_URL;
+
+  if (url) {
+    url = url.startsWith("http") ? url : `https://${url}`;
+    url = url.endsWith("/") ? url : `${url}/`;
+  }
+
+  return url;
+};
